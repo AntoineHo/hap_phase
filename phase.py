69bd2a121c8949a9c4b102f0fdb41fdeddef9b4b
Antoine Houtain <antoine.houtain@gmail.com>
2022-08-23 13:20:04 +0200

    Add files via upload

--- phase.py
+++ phase.py
@@ -202,8 +202,10 @@ def write_new_vcf(output, phased_vcf, new_sequences, only_phased) : # Need pysam
         lines = vcf_df.query("ctg == @chrom & pos >= @start & pos <= @end")
         # update the coordinates
         data = lines.copy()
+        print(data.head())
         data.loc[:]["pos"] = data.loc[:]["pos"] - (start-1) # since start is 1-based, we must substract start-1
         data.loc[:]["ctg"].replace({chrom:new_sequence_name}, inplace=True)
+        print(data.head())
         new_blocks.append(data)
 
         i += 1
@@ -315,7 +317,7 @@ def phase(args) :
 
         log("{} ({}/{}): Outputting haploid fasta of regions to phase...".format(sm, i, t)) # sample wise
         #outfa = "{}.fa".format(output)
-        fa_out = os.path.join(outdir, sm+".fa")
+        fa_out = os.path.join(outdir, sm + ".fa")
         new_seqs = write_new_fasta(fa_out, refseqs, only_phased)
 
         log("{} ({}/{}): Writing a new phased .VCF with per-region information...".format(sm, i, t)) # sample wise
